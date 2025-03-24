48f405b1d3f42df3d4dfbd7ae3ab701f2db2214a
Tataru Robert <robert.tataru98@gmail.com>
2025-03-24 22:56:45 +0200

    Update Name

--- Sources/PendoIOAnalyticsConsumer/PendoIOAnalyticsConsumer.swift
+++ Sources/PendoIOAnalyticsConsumer/PendoIOAnalyticsConsumer.swift
@@ -22,8 +22,6 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 */
 
-// The Swift Programming Language
-// https://docs.swift.org/swift-book
 
 import TAAnalytics
 
--- Tests/PendoIOAnalyticsConsumerTests/PendoIOAnalyticsConsumerTests.swift
+++ Tests/PendoIOAnalyticsConsumerTests/PendoIOAnalyticsConsumerTests.swift
@@ -23,7 +23,7 @@ SOFTWARE.
 */
 
 import Testing
-@testable import TAAnalyticsPendoIOConsumer
+@testable import PendoIOAnalyticsConsumer
 
 @Test func example() async throws {
     // Write your test here and use APIs like `#expect(...)` to check expected conditions.
